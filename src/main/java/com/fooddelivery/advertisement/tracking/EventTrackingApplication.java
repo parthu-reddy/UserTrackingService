ea9d6460df33ee908509eea0eadbf42176b451f1
Parthu <parthudhanireddy@gmail.com>
2026-08-22 15:37:55 +0530

    Fix schema validation and configuration errors

--- src/main/java/com/fooddelivery/advertisement/tracking/EventTrackingApplication.java
+++ src/main/java/com/fooddelivery/advertisement/tracking/EventTrackingApplication.java
@@ -3,7 +3,9 @@ package com.fooddelivery.advertisement.tracking;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = {"com.fooddelivery.advertisement.tracking", "com.fooddelivery"})
+@SpringBootApplication(
+    scanBasePackages = {"com.fooddelivery.advertisement.tracking", "com.fooddelivery.common"}
+)
 @org.springframework.context.annotation.Import({com.fooddelivery.common.security.CommonSecurityConfig.class, com.fooddelivery.common.security.SecurityContextFilter.class})
 public class EventTrackingApplication {
     public static void main(String[] args) {
